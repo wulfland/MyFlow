55eb1d878be1101b0c2ce3292624b00e5b86caf8
Michael Kaufmann <5276337+wulfland@users.noreply.github.com>
2021-02-26 09:15:50 +0100

    Add sample code and tests
    This commit adds a simple extension and some test to the codebase.
    Run
    
    dotnet test
    
    in the root to run the tests.

---
+++ tests/ToRomanNumeralsTests.cs
@@ -0,0 +1,31 @@
+using System;
+using Xunit;
+using ToRomanNumerals;
+
+namespace ToRomanNumeralsTests
+{
+    public class ToRomanNumeralsTests
+    {
+        [Theory]
+        [InlineData(-1, "")]
+        [InlineData(0, "")]
+        [InlineData(1, "I")]
+        [InlineData(2, "II")]
+        [InlineData(3, "III")]
+        [InlineData(4, "IV")]
+        [InlineData(5, "V")]
+        [InlineData(9, "IX")]
+        [InlineData(10, "X")]
+        [InlineData(11, "XI")]
+        [InlineData(12, "XII")]
+        [InlineData(42, "XLII")]
+        [InlineData(99, "XCIX")]    
+        [InlineData(2013, "MMXIII")]
+        public void ToRomanNumeralsTest(int input, string expected)
+        {
+            string result = input.ToRomanNumerals();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
